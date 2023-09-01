0496f2241ae767d4539686d8d8c55ee4279e88fe
Matthew Barclay <matthewbarclay1@icloud.com>
2023-09-01 14:01:28 +0100

    loops

---
+++ JS/loops.js
@@ -0,0 +1,22 @@
+let i = 1;
+
+for (i; i < 10; i++) {
+    console.log(i);
+}
+
+let x = 2;
+let loopCounter = 0;
+
+while (x < 10000) {
+    x **= 2
+    loopCounter++
+    console.log(x, loopCounter)
+
+}
+
+i = 10;
+
+for (i; i > 0; i--) {
+    console.log(i);
+}
+
